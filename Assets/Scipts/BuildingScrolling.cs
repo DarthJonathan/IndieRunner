7775c20d19f9266d66296dea9ac26f815e9c9c94
Jonathan Hosea <jonathan.hosea@me.com>
2017-04-19 13:47:10 +0700

    Moving building parallax effect (need more buildings)

--- Assets/Scipts/BuildingScrolling.cs
+++ Assets/Scipts/BuildingScrolling.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class BuildingScrolling : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	Vector3 startPlace;
+	public float speed;
+
+	void Start ()
+	{
+		startPlace = new Vector3 (40, transform.position.y, transform.position.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (transform.position.x < -18 && Player.isAlive) {
+			transform.position = startPlace;
+		} else if(Player.isAlive){
+			transform.position = new Vector3 (transform.position.x - speed, transform.position.y, transform.position.z);
+		}
 		
 	}
 }
