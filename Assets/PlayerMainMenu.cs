defa63bab26d12dec899eb77faa86cb318a53730
Jonathan Hosea <jonathan.hosea@me.com>
2017-04-19 18:03:21 +0700

    MainMenu Adjustment

--- Assets/PlayerMainMenu.cs
+++ Assets/PlayerMainMenu.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerMainMenu : MonoBehaviour {
 
 	public static bool isAlive = true;
+	Vector3 pos;
 
 	// Use this for initialization
 	void Start () {
+
+		pos = Camera.main.WorldToViewportPoint(transform.position);
 		
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+
+		transform.position = new Vector3 (pos.x + 60, transform.position.y, transform.position.z);
 		
 	}
 }
