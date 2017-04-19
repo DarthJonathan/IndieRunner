2c1ee9fd17a1a811a835af6b1056c5dc1b44b315
halle10@coventry.ac.uk <halle10@coventry.ac.uk>
2017-04-19 06:39:11 +0100

    Organised assets into folders. Also made it so you can now move while jumping.

---
+++ Assets/Scipts/Player.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private Vector3 position;
+
+    public float powerUpTime = 5;
+    private bool isPoweredUp = false;
+	private float powerupPassed = 0;
+
+	public static bool isAlive;
+	public static float playerScore;
+
+	private bool invincible = false;
+	private float stopJump = 0;
+	public float jumpLength;
+	private bool hopping = false;
+	Animator animator;
+
+    private Vector3 endPos;
+    private float timer = 0;
+
+    private lanes lane;
+
+	// Use this for initialization
+	void Start () {
+		position = transform.position;
+		isAlive = true;
+		Time.timeScale = 1f;
+		playerScore = 0;
+
+		animator = GetComponent<Animator> ();
+        endPos = transform.position;
+
+        lane = lanes.lane2;
+    }
+
+    void FixedUpdate ()
+	{
+		if (isPoweredUp) {
+
+			Destroy(GameObject.FindGameObjectWithTag("PowerUp"));
+
+			if (powerupPassed > powerUpTime) {
+				Time.timeScale = 1f;
+				isPoweredUp = false;
+				powerupPassed = 0;
+			} else {
+				Time.timeScale = 0.5f;
+				powerupPassed += Time.deltaTime*2;
+			}
+		}
+
+		playerScore += Time.deltaTime*10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.position = position;
+
+        if (lane == lanes.lane1)
+        {
+            position = new Vector3(position.x, 5, position.z);
+        }
+
+        if (lane == lanes.lane2)
+        {
+            position = new Vector3(position.x, 0, position.z);
+        }
+
+        if (lane == lanes.lane3)
+        {
+            position = new Vector3(position.x, -5, position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            switch(lane)
+            {
+                case lanes.lane1:
+                    break;
+                case lanes.lane2:
+                    lane = lanes.lane1;
+                    break;
+                case lanes.lane3:
+                    lane = lanes.lane2;
+                    break;
+                default:
+                    lane = lanes.lane2;
+                    break;
+            }               
+        }
+
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            switch (lane)
+            {
+                case lanes.lane1:
+                    lane = lanes.lane2;
+                    break;
+                case lanes.lane2:
+                    lane = lanes.lane3;
+                    break;
+                case lanes.lane3:
+                    break;
+                default:
+                    lane = lanes.lane2;
+                    break;
+            }
+        }
+
+		if (Input.GetKey(KeyCode.RightArrow) && position.x < 23)
+        {
+                position = new Vector3(position.x + 15 * Time.deltaTime, position.y, position.z);
+        }
+
+		if (Input.GetKey(KeyCode.LeftArrow) && position.x > -5)
+        {
+                position = new Vector3(position.x - 15 * Time.deltaTime, position.y, position.z);
+        }
+
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && !invincible)
+		{
+			invincible = true;
+			stopJump = Time.time + jumpLength;
+			animator.SetTrigger ("isJumping");
+
+            timer = 0.0f;
+            endPos = position;
+
+        }
+        if (timer <= 1)
+        {
+            timer += Time.deltaTime / 0.75f;
+            var height = Mathf.Sin(Mathf.PI * timer) * 4f;
+            position = Vector3.Lerp(position, endPos, timer) + Vector3.up * height;
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) && position.y < 5)
+            {
+                endPos = new Vector3(position.x, position.y + 5 * Time.deltaTime, position.z);
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow) && position.y > -5)
+            {
+                endPos = new Vector3(position.x, position.y - 5 * Time.deltaTime, position.z);
+            }
+            if (Input.GetKey(KeyCode.RightArrow) && position.x < 23)
+            {
+                endPos = new Vector3(position.x + 15 * Time.deltaTime, position.y - height, position.z);
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow) && position.x > -5)
+            {
+                endPos = new Vector3(position.x - 15 * Time.deltaTime, position.y - height, position.z);
+            }
+        }
+
+        if (invincible && Time.time > stopJump)
+		{
+			invincible = false;
+		}
+    }
+
+	void OnGUI ()
+	{
+		//Jumping counter
+		if (Time.time < stopJump && invincible) {
+			GUI.Box(new Rect(Screen.width/2 - 40,20,80,50), (stopJump - Time.time).ToString("f0"));
+		}
+	}
+
+//	If collided with something
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.tag == "Enemy" && !invincible) {
+//			Debug.Log ("Collided With Enemy");
+			isAlive = false;
+		} else if(collider.tag == "PowerUp"){
+//			Debug.Log ("Collided With PowerUp");
+			isPoweredUp = true;
+		}
+	}
+
+    public enum lanes
+    {
+        lane1,
+        lane2,
+        lane3
+    }
+}
