0746b4ba1681d5717a9e7300d71a8caec6acdd1b
Jonathan Hosea <jonathan.hosea@me.com>
2017-04-20 11:19:21 +0700

    main menu background music

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,6 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour {
 
+	AudioSource source;
+	public AudioClip bgm;
+	public AudioClip traffic;
+
+	void Start()
+	{
+		source = GetComponent<AudioSource> ();
+		source.PlayOneShot (bgm);
+		source.PlayOneShot (traffic);
+	}
+
+	void update()
+	{
+		if (source.time == traffic.length) {
+			source.PlayOneShot (bgm);
+			source.PlayOneShot (traffic);
+		}
+	}
+
 	public void StartGame ()
 	{
 		SceneManager.LoadScene ("GamePlay");
